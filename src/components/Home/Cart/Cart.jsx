bf05d370df2d5aa41df755c4163b4ab4f0f7d0f1
kazimehedihasan <kazimehedihasan458@.com>
2023-09-14 23:28:59 +0600

    first commit

--- src/components/Home/Cart/Cart.jsx
+++ src/components/Home/Cart/Cart.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable react/prop-types */
+
 import './Cart.css'
-const Cart = () => {
+const Cart = ({selectActor, remaining, totalCoust}) => {
+    // console.log(selectActor);
     return (
         <div>
-              <h2>hello world</h2>
+              <h2>total actors: {selectActor.length}</h2>
+              <h5>remaining: {remaining}</h5>
+              <h5>totalCoust: {totalCoust}</h5>
+              {selectActor.map((acto) => (
+                    <li key={acto.id}>{acto.name}</li>
+                
+                ))}
         </div>
+        
     );
 };
-
 export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,28 +1,72 @@
-import { useEffect } from "react";
+/* eslint-disable react/jsx-key */
+import { useEffect, useState } from "react";
 import "./Home.css";
-const Home = () => {
+import Cart from "./Cart/Cart";
 
+
+
+const Home = () => {
+const [allactors, aseAllactors] = useState([]);
+const [selectActor, setSelectactors] = useState([]);
+const [remaining, setRemaining] = useState(0);
+const [totalCoust, setTotalCoust] = useState(0);
     useEffect(() => {
         fetch("./data.json")
         .then((res  => res.json()))
-        .then((data) => console.log(data))
+        .then((data) => aseAllactors(data))
     },[])
+
+    // select button kaj
+const handleSelectorActors = (actor)=> {
+  const isExist = selectActor.find((item) => item.id === actor.id);
+  let count = actor.salary;
+  if (isExist) {
+    return alert ('Selector actor ')
+  }
+else{
+  selectActor.forEach((item) =>{
+    count = count + item.salary;
+  })
+  // console.log(count);
+  const totalRemaining = 20000 - count;
+  if (count > 20000) {
+    return alert ('mama taka ses tumi akhon gumao');
+  }
+ else{
+  setTotalCoust(count);
+
+  setRemaining(totalRemaining);
+  setSelectactors([...selectActor, actor])
+ }
+}
+
+}
+    // console.log(allactors);
+
   return (
     <div className="container">
-      <div className="card-containner">
-        <div className="card">
+      <div className="home-containner">
+        <div className="card-container ">
+          {
+            allactors.map((actor) =>(
+<div key={actor.id} className="card">
           <div className="card-img">
-            <img className="photo" src="https://i.ibb.co/Rvc67Cr/ar.jpg" alt="" />
+            <img className="photo" src={actor.image} alt="" />
           </div>
-          <h2>kazi mehedi</h2>
-          <p><small>Lorem ipsum dolor sit amet,
-             consectetur adipisicing elit.
-              Molestiae, doloremque.</small></p>
+          <h2>{actor.name}</h2>
+          <p><small>{actor.role}</small></p>
               <div className="info">
-                <p>salary: 200</p>
+                <p>{actor.salary} $</p>
                 <p>writer</p>
               </div>
-              <button className="select-btn">Select</button>
+              <button onClick={()=> handleSelectorActors(actor)} className="select-btn">Select</button>
+        </div>
+
+            ))
+          }
+        </div>
+        <div className="cart">
+       <Cart selectActor={selectActor} remaining={remaining} totalCoust={totalCoust}></Cart>
         </div>
       </div>
     </div>
